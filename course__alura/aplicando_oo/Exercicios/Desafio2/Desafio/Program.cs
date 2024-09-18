d0617d4de509d2eda790a2a1faf9e8d64344be9a
Ronan Ferraz <ronanbenitis@hotmail.com>
2024-09-18 17:35:52 -0300

    Avançando no curso de POO

---
+++ course__alura/aplicando_oo/Exercicios/Desafio2/Desafio/Program.cs
@@ -0,0 +1,12 @@
+﻿using Desafio.model;
+
+ContaBancaria conta1 = new("123", "Jorge Benjor", 123123123, "Aaaahey123");
+conta1.ExibirInformacoes();
+
+Carro carro1 = new("Wolskvagen", "Gol");
+carro1.Ano = 2024;
+carro1.Acelerar();
+carro1.Frear();
+carro1.Buzinar();
+Console.WriteLine(carro1.DescricaoDetalhada);
+// FALTOU FAZER A CLASSE PRODUTO
